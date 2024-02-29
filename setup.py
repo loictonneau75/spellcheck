809b557bb60a060781adb3cc62b5e3b3c0a64088
Loic tonneau <loictonneau75@gmail.com>
2024-02-29 18:39:24 +0100

    try

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DESCRITPION = "SpellCheck is a Python library designed to check and correct spel
 LONG_DESCRIPTION = "SpellCheck is a Python library designed to check and correct spelling errors in texts across multiple languages using OpenAI's large language models (LLMs). It leverages the `langchain_openai.ChatOpenAI` interface to offer a seamless integration for developers looking to enhance their applications with advanced spell checking capabilities."
 
 setup(
-    name = "spellchecker",
+    name = "SpellCheck",
     version = VERSION,
     author = "Loïc TONNEAU",
     author_email = "loictonneau@gmail.com",
